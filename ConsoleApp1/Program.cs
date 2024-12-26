a3262b3f32997c0b275ba64fb316153236809e8e
dwightm1 <113989056+Mikami2208@users.noreply.github.com>
2024-12-26 10:06:36 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,74 @@
+﻿
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string folderPath = "/Users/artemdwightm1/Documents/projects/Новая папка";
+        List<string> noFileList = new List<string>();
+        List<string> badFileList = new List<string>();
+        List<string> overflowList = new List<string>();
+
+        int sumOfProducts = 0;
+        int count = 0;
+
+        List<string> fileNames = new List<string>();
+
+        try
+        {
+            File.WriteAllText(folderPath + "/no_file.txt", string.Empty);
+            File.WriteAllText(folderPath + "/bad_file.txt", string.Empty);
+            File.WriteAllText(folderPath + "/overflow_file.txt", string.Empty);
+
+            for(int i = 0; i <= 19; i++)
+            {
+                fileNames.Add(folderPath + $"/{i+10}.txt");
+                try
+                {
+                    string[] numbers = File.ReadAllLines(fileNames[i]);
+
+                    int line1 = int.Parse(numbers[0]);
+                    int line2 = int.Parse(numbers[1]);
+
+                    long multiply = line1 * line2;
+                    int sumInt = checked((int)multiply);
+
+                    sumOfProducts += sumInt;
+                    count++;
+
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    badFileList.Add(fileNames[i]);
+                }
+                catch (FileNotFoundException)
+                {
+                    noFileList.Add(fileNames[i]);
+                }
+                catch (FormatException)
+                {
+                    badFileList.Add(fileNames[i]);
+                }
+                catch (OverflowException)
+                {
+                    overflowList.Add(fileNames[i]);
+                }
+                
+            }
+            File.WriteAllLines(folderPath + "/no_file.txt", noFileList);
+            File.WriteAllLines(folderPath + "/bad_file.txt", badFileList);
+            File.WriteAllLines(folderPath + "/overflow_file.txt", overflowList);
+           
+            double average = (double)sumOfProducts / count;
+            Console.WriteLine(average);
+            
+        }
+        catch (UnauthorizedAccessException)
+        {
+            Console.WriteLine("Неможливо створити або оновити файл no_file.txt, bad_data.txt або overflow.txt.");
+            return;
+        }
+
+
+    }
+}
---
+++ lab3_2kyrs/Program.cs
@@ -0,0 +1,50 @@
+﻿using SixLabors.ImageSharp;
+using System.Text.RegularExpressions;
+using SixLabors.ImageSharp.Processing;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string folderPath = "/Users/artemdwightm1/Documents/Новая папка 2";
+        Regex regexExtForImage = new Regex(@"^((bmp)|(gif)|(tiff?)|(jpe?g)|(png))$", RegexOptions.IgnoreCase);
+
+        foreach (string filePath in Directory.GetFiles(folderPath))
+        {
+            string extension = Path.GetExtension(filePath).TrimStart('.').ToLower();
+            string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(filePath);
+
+            try
+            {
+                if (regexExtForImage.IsMatch(extension) && !fileNameWithoutExtension.EndsWith("-mirrored"))
+                {
+                    using (Image orgIm = Image.Load(filePath))
+                    {
+                        orgIm.Mutate(x => x.Flip(FlipMode.Horizontal));
+                        string newFile = $"{folderPath}/{fileNameWithoutExtension}-mirrored.gif";
+                        orgIm.Save(newFile);
+                    }
+                }
+                else if (fileNameWithoutExtension.EndsWith("-mirrored"))
+                {
+                    Console.WriteLine($"Файл {filePath} вже відредагований");
+                }
+                else
+                {
+                    Console.WriteLine($"Файл {filePath} не є графічним.");
+                }
+            }
+            catch (Exception ex)
+            {
+                if (regexExtForImage.IsMatch(extension))
+                {
+                    Console.WriteLine($"Файл {filePath} не містить картинку, хоча має графічне розширення:  {ex.Message}");
+                }
+                else
+                {
+                    Console.WriteLine($"Файл {filePath} нне є графічним.");
+                }
+            }
+        }
+    }
+}
